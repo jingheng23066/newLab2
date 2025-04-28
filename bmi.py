df39f388c77d12d5782539760bccb421326a7a3a
jingheng <jingheng.23@ichat.sp.edu.sg>
2025-04-28 15:05:23 +0800

    commiting

--- bmi.py
+++ bmi.py
@@ -1,5 +1,19 @@
 def calculate_bmi(height, weight):
- print("Height = " + height)
- print("Weight = " + weight)
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    
+    bmi = weight / (height * height)
+    
+    print("BMI = " + str(bmi))
 
- calculate_bmi(weight="57", height="1.73")
+    if bmi < 18.5:
+        print("BMI Range Classification: Under Weight")
+        return -1 
+    elif 18.5 <= bmi <= 25.0: 
+        print("BMI Range Classification: Normal Weight")
+        return 0
+    else:
+        print("BMI Range Classification: Over Weight")
+        return 1
+
+calculate_bmi(weight=57, height=1.73)
